16a46c4da4b39ae9bfd18bf950d44cd435f34e8d
RiyanshRag <109984065+RiyanshRag@users.noreply.github.com>
2022-11-16 21:44:55 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var player
+function preload(){
+    player_running = loadAnimation(,"trex3.png","trex4.png");
+    player_jumping = loadAnimation("trex_collided.png");
+}
+
+function setup() {
+ 
+}
+
+function draw() {
+ 
+}
